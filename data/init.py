551dd327a5c53038700b3838d5fb046a101de879
roberta039 <165647793+roberta039@users.noreply.github.com>
2025-11-26 15:45:55 +0200

    Create init.py

---
+++ data/init.py
@@ -0,0 +1,4 @@
+"""Data package initialization."""
+from .airports import AIRPORTS, get_countries_by_continent, get_airports_by_country
+
+__all__ = ['AIRPORTS', 'get_countries_by_continent', 'get_airports_by_country']
