ee71f2130c6a76d55bddbf73440c86d225b8da8a
roberta039 <165647793+roberta039@users.noreply.github.com>
2025-11-28 15:50:06 +0200

    Update init.py

--- services/init.py
+++ services/init.py
@@ -1,5 +1,5 @@
 """Services package initialization."""
-from .flight_apis import FlightAggregator, AmadeusAPI
+from .flight_apis import FlightAggregator
 from .cache_manager import cache_manager
 
-__all__ = ['FlightAggregator', 'AmadeusAPI', 'cache_manager']
+__all__ = ['FlightAggregator', 'cache_manager']
